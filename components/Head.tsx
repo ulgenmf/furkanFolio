9a3bca9d606e77c0ce945dde4066b09e46aa57c8
0101110101 <51328751+0101110101@users.noreply.github.com>
2022-10-16 18:01:04 +0200

    first

---
+++ components/Head.tsx
@@ -0,0 +1,16 @@
+import Head from "next/head";
+
+export function Head() {
+	return (
+		<>
+			<Head>
+				<link rel="preconnect" href="https://fonts.googleapis.com" />
+				<link rel="preconnect" href="https://fonts.gstatic.com" />
+				<link
+					href="https://fonts.googleapis.com/css2?family=Fuzzy+Bubbles:wght@700&display=swap"
+					rel="stylesheet"
+				/>
+			</Head>
+		</>
+	);
+}
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+	theme: {
+		extend: {
+			fontFamily: {
+				Major: ["Major", "monospace"],
+				Motonotn: ["Monoton", " cursive"],
+				abc: ["Fuzzy Bubbles", "cursive"],
+			},
+			colors: {
+				steam: "#10141F",
+			},
+		},
+	},
+	plugins: [],
+};
